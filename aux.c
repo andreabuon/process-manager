e497ba072df923f37b7821d625f57844bf22867f
Andrea Buonaurio <65712954+andreabuon@users.noreply.github.com>
2022-06-20 00:43:03 +0200

    Risolto Bug Memoria (causato da dichiarazione sbagliata delle stringhe "/proc/" e "/stat") Separata funzione per assemblare stringa path

--- aux.c
+++ aux.c
@@ -11,14 +11,15 @@
 #include <string.h>
 
 #define CHUNK 1024 //numero di byte letti per volta
+#define PATH_LEN 256
 
 void getCPUinfo() {
 	FILE *file = fopen("/proc/cpuinfo", "r");
 	assert(file && "Errore apertura file");
 
-	char c[CHUNK];
-	while (fgets(c, CHUNK, file)){
-		printf("%s", c);
+	char read_chars[CHUNK];
+	while (fgets(read_chars, CHUNK, file)){
+		printf("%s", read_chars);
 	}
 	fclose(file);
 }
@@ -32,41 +33,45 @@ void getProcessesList(){
 	assert(entry && "Errore lettura directory");
 
 	while(entry){
-		//stampa solo le entry di directory che corrispondono a processi (ovvero che hanno come nome un numero [il pid])
+		// Stampa solo le entry delle directory che corrispondono a processi 
+		// ovvero quelle che hanno come nome un numero [il pid]
 		if(entry->d_type == DT_DIR && isNumber(entry->d_name)){
-			printf(" "); //commentando crasha il programma?!?
-			info* info = getProcessInfo(entry->d_name);
-			info_print(info);
-			info_free(info);
+			info* process_info = getProcessInfo(entry->d_name);
+			info_print(process_info);
+			info_free(process_info);
 		}
 		entry = readdir(dir);
 	}
 	closedir(dir);
 }
 
-info* getProcessInfo(char *pid){
-	//Le informazioni sul processo vengono estratte dal file: "/proc/[pid]/stat"
-	char path[50]; //50 numero a caso, dovrebbe bastare; sistemare
-	char dir[] = "/proc/"; 
-	char filename[] = "/stat";
-	memset(path, 0, 50);
-	strcat(path, dir); //sistemare n
+// Dato un PID assembla e restituisce la stringa: "/proc/[pid]/stat"
+char* buildPathByPID(const char* pid){
+	char *path = calloc(PATH_LEN, sizeof(char));
+	strcat(path, "/proc/");
 	strcat(path, pid);
-	strcat(path, filename);
+	strcat(path, "/stat");
+	return path;
+}
+
+info* getProcessInfo(const char *pid){
+	char* path = buildPathByPID(pid);
 
 	FILE *file = fopen(path, "r");
 	assert(file && "Errore apertura file");
 
 	char* line = NULL;
 	size_t n;
-	getline(&line, &n, file);
+	int read_chars = getline(&line, &n, file);
+	assert(read_chars>0 && "Errore lettura file stat del processo");
 
-	info* info = info_new();
-	sscanf(line, "%d %s %c", &(info->pid), &(info->comm), &(info->state)); //sistemare
+	info* process_info = info_new();
+	sscanf(line, "%d %s %c", &(process_info->pid), &(process_info->comm), &(process_info->state)); //sistemare
 
 	free(line);
+	free(path);
 	fclose(file);
-	return info;
+	return process_info;
 }
 
 int sendSignal(int signal){
@@ -75,7 +80,7 @@ int sendSignal(int signal){
 	kill(pid, signal);
 }
 
-int isNumber(char* string){
+int isNumber(const char* string){
 	while(*string){
 		if(!isdigit(*string))
 			return 0;
@@ -85,14 +90,14 @@ int isNumber(char* string){
 }
 
 info* info_new(){
-	info* info = malloc(sizeof(info));
-	return info;
+	info* process_info = malloc(sizeof(info));
+	return process_info;
 }
 
-void info_print(info* info){
-	printf("%d %s %c\n", info->pid, info->comm, info->state);
+void info_print(const info* process_info){
+	printf("%d %s %c\n", process_info->pid, process_info->comm, process_info->state);
 }
 
-void info_free(info* info){
-	free(info);
+void info_free(info* process_info){
+	free(process_info);
 }
--- aux.h
+++ aux.h
@@ -1,28 +1,28 @@
-//Struttura dati che contiene le informazioni che ci interessano sul processo
+//Struttura dati che contiene le informazioni che interessano del processo
 typedef struct info{
 	int pid;
-	char comm[17]; //sistemare, dovrebbe essere 16+1
+	char comm[17];
 	char state;
 } info;
-//Alloca una nuova struttura info
+
+//Alloca una nuova struttura info e ne restituisce il puntatore
 info* info_new();
+//Dealloca la struttura info puntata dal puntatore in input
+void info_free(info* process_info);
 //Stampa su console i dati del processo salvati nella struttura info in input
-void info_print(info* info);
-//Dealloca struttura info in input
-void info_free(info* info);
+void info_print(const info* process_info);
 
 //Stampa su console le informazioni relative al processore
 void getCPUinfo();
 
 //Stampa su console l'elenco dei processi in esecuzione.
-//Viene stampata una riga per ogni cartella nel fs /proc/ che come nome ha un valore numerico (pid)
 void getProcessesList();
 
-//Crea una nuova struttura info con le informazioni relative al processo il cui pid è passato in input
-info* getProcessInfo(char* pid);
+//Crea una nuova struttura info con le informazioni relative al processo (il cui pid è passato in input) e ne ritorna il puntatore.
+info* getProcessInfo(const char* pid);
 
-//Controlla se la stringa passata come argomento contiene solo numeri
-int isNumber(char* string);
+//Controlla se la stringa passata come argomento è composta esclusivamente da numeri
+int isNumber(const char* string);
 
 //wrapper per mandare segnali ad un processo
 //verrà collegato ai pulsanti nella gui
