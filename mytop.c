1fc5e35564bbe70c93fa9e88126c4faed3647fa0
Andrea Buonaurio <65712954+andreabuon@users.noreply.github.com>
2022-06-20 21:14:48 +0200

    Aggiunta possibilità di scrollare nella lista Processi

--- mytop.c
+++ mytop.c
@@ -16,11 +16,11 @@ static void activate(GtkApplication *app, gpointer user_data)
 	//g_signal_connect(btn_kill, "clicked", G_CALLBACK(getCPUinfo), NULL); //sistemare
 
 	GObject *treeview = gtk_builder_get_object(builder, "treeview");
-	GtkListStore *liststore = gtk_list_store_new(4, G_TYPE_STRING, G_TYPE_INT, G_TYPE_CHAR, G_TYPE_STRING); //sistemare, non visualizza il carattere ma int corrispondente
+	GtkListStore *liststore = gtk_list_store_new(4, G_TYPE_STRING, G_TYPE_INT, G_TYPE_CHAR, G_TYPE_INT); //sistemare, non visualizza il carattere ma int corrispondente
 	GtkTreeIter iter;
-	for(int i=0; i<10; i++){
+	for(int i=0; i<40; i++){
 		gtk_list_store_append(liststore, &iter);
-		gtk_list_store_set(liststore, &iter, 0, "Processo", 1, i, 2, "R", 3, "100 MB", -1);
+		gtk_list_store_set(liststore, &iter, 0, "Processo", 1, i, 2, "R", 3, 100, -1);
 	}
 	gtk_tree_view_set_model((GtkTreeView*) treeview, (GtkTreeModel*) liststore);
 
