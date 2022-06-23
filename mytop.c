9c5cd090b5cbc0219db675a071036144459774f1
Andrea Buonaurio <65712954+andreabuon@users.noreply.github.com>
2022-06-23 18:49:32 +0200

    Bottoni GUI mandano segnale al processo selezionato

--- mytop.c
+++ mytop.c
@@ -1,6 +1,9 @@
 #include <gtk/gtk.h>
 
 #include "process.h"
+#include <signal.h>
+
+GObject *treeview; //sistemare
 
 void aggiungiProcessi(GtkListStore *liststore){
 	GtkTreeIter iter;
@@ -14,6 +17,39 @@ void aggiungiProcessi(GtkListStore *liststore){
 	}
 }
 
+int getSelectedProcessPID(){
+	GtkTreeSelection* treeSelection = gtk_tree_view_get_selection((GtkTreeView*) treeview);
+	GtkTreeModel* model;
+	GtkTreeIter iter;
+	pid_t pid;
+
+	gtk_tree_selection_get_selected(treeSelection, &model, &iter);
+	gtk_tree_model_get(model, &iter, 1, &pid, -1);
+	//printf("Click! PID: %d", pid);
+	//fflush(stdout);
+	return pid;
+}
+
+void killProcess(){
+	int pid = getSelectedProcessPID();
+	kill(pid, SIGKILL);
+}
+
+void terminateProcess(){
+	int pid = getSelectedProcessPID();
+	kill(pid, SIGTERM);
+}
+
+void suspendProcess(){
+	int pid = getSelectedProcessPID();
+	kill(pid, SIGSTOP);
+}
+
+void resumeProcess(){
+	int pid = getSelectedProcessPID();
+	kill(pid, SIGCONT);
+}
+
 static void activate(GtkApplication *app, gpointer user_data)
 {
 	/* Construct a GtkBuilder instance and load our UI description */
@@ -24,14 +60,23 @@ static void activate(GtkApplication *app, gpointer user_data)
 	GObject *window = gtk_builder_get_object(builder, "window");
 	gtk_window_set_application(GTK_WINDOW(window), app);
 	
-	//GObject *btn_kill = gtk_builder_get_object(builder, "btn_kill");
-	//g_signal_connect(btn_kill, "clicked", G_CALLBACK(), NULL); //sistemare
-
-	GObject *treeview = gtk_builder_get_object(builder, "treeview");
+	treeview = gtk_builder_get_object(builder, "treeview");
 	GtkListStore *liststore = gtk_list_store_new(4, G_TYPE_STRING, G_TYPE_INT, G_TYPE_CHAR, G_TYPE_INT); //sistemare, non visualizza il carattere ma int corrispondente
 	
 	aggiungiProcessi(liststore);
 	gtk_tree_view_set_model((GtkTreeView*) treeview, (GtkTreeModel*) liststore);
+
+	GObject *btn_kill = gtk_builder_get_object(builder, "btn_kill");
+	g_signal_connect(btn_kill, "clicked", G_CALLBACK(killProcess), NULL);
+
+	GObject *btn_terminate = gtk_builder_get_object(builder, "btn_terminate");
+	g_signal_connect(btn_terminate, "clicked", G_CALLBACK(terminateProcess), NULL);
+
+	GObject *btn_suspend = gtk_builder_get_object(builder, "btn_suspend");
+	g_signal_connect(btn_suspend, "clicked", G_CALLBACK(suspendProcess), NULL);
+
+	GObject *btn_resume = gtk_builder_get_object(builder, "btn_resume");
+	g_signal_connect(btn_resume, "clicked", G_CALLBACK(resumeProcess), NULL);
 
 	gtk_widget_show(GTK_WIDGET(window));
 
