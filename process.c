75d47e1d006a26047d19849474cc9bb3cac4599d
Andrea Buonaurio <65712954+andreabuon@users.noreply.github.com>
2022-07-16 18:27:38 +0200

    Separata funzione per parsing dati processi

--- process.c
+++ process.c
@@ -42,85 +42,43 @@ void info_print(const info* process_info){
 	printf("%d %s %d %ld\n", process_info->pid, process_info->command, process_info->cpu_usage, process_info->memory);
 }
 
-info* getProcessInfo(const int dir_fd){
-	//Apertura file "stat" nella directory dir_fd (in input). dir_fd = /proc/[pid]
-	int stat_fd = openat(dir_fd, "stat", O_RDONLY);
-	if(stat_fd == -1){
-		perror("getProcessInfo: Errore openat stat_fd");
-		return NULL;
-	}
-
-	FILE* file = fdopen(stat_fd, "r");
-	if(!file){
-		perror("getProcessInfo: Errore fdopen file");
-		close(stat_fd);
-		return NULL;
-	}
+//Esegue il parsing dei dati del processo dal file e li salva nella struttura process_info. Ritorna 0 in caso di successo e 1 in caso di errore.
+int parseData(FILE* file, info* process_info){
+	int ret;
 
 	pid_t pid = 0; 
 	char* comm;
 	unsigned flags;
 	float cpu_usage;
-	long mem; //leggi dopo
+	long mem;
 
-	//variabili necessarie per calcore l'uso della cpu
+	//variabili necessarie per calcolo uso della cpu
 	long unsigned utime, stime; //espresso in clock ticks
 	long long unsigned starttime; //espresso in secondi
 	long ticks = sysconf(_SC_CLK_TCK); //numero clock ticks al secondo
 
 	/* Parametri SCANF - per altre info consultare "man 5 proc"
 	(1) %d pid -> PID del processo
-	(2) (%m[^)]) comm -> Nome dell'eseguibile del processo, togliendo la parentesi tonda iniziale e finale. Alloca automaticamente la memoria necessaria per contenere la stringa e il null terminator. Il null terminator viene aggiunto automaticamente. Supporta anche nomi che contengono spazi (al contrario di %s) //FIXME errore su processo (sd-pam)
+	(2) (%m[^)]) comm -> Nome dell'eseguibile del processo, togliendo la parentesi tonda iniziale e finale. Alloca automaticamente la memoria necessaria per contenere la stringa e il null terminator. Il null terminator viene aggiunto automaticamente. Supporta anche nomi che contengono spazi (a differenza di "%s") //FIXME processo (sd-pam) provoca errore
 	(9) %u flags
 	(14) %lu utime
 	(15) %lu stime
 	(22) %llu starttime  
+	(24) %ld rss -> memoria residente
 	(-) %* -> valori ignorati
-	(24) %ld -> memoria residente
 	*/
-	int ret;
-	ret = fscanf(file, "%d (%m[^)]) %*c %*d %*d %*d %*d %*d %u %*u %*u %*u %*u %lu %lu %*d %*d %*d %*d %*d %*d %llu %*u %ld", &pid, &comm, &flags, &utime, &stime, &starttime, &mem);
+	char* format_string = "%d (%m[^)]) %*c %*d %*d %*d %*d %*d %u %*u %*u %*u %*u %lu %lu %*d %*d %*d %*d %*d %*d %llu %*u %ld";
+	ret = fscanf(file, format_string, &pid, &comm, &flags, &utime, &stime, &starttime, &mem);
 	if(ret==EOF || ret < 7){
 		if(ret == EOF)
-			perror("getProcessInfo: Errore fscanf");
+			perror("parseData: Errore fscanf");
 		else
-			fprintf(stderr, "getProcessInfo: Errore matching\n");
-		fprintf(stderr, "getProcessInfo: Errore lettura info del processo");
+			fprintf(stderr, "parseData: Errore matching\n");
+		fprintf(stderr, "parseData: Errore lettura info del processo");
 		if(pid)
 			fprintf(stderr, " [%d]", pid);
 		fprintf(stderr, "\n");
-		free(comm);
-		fclose(file);
-		close(stat_fd);
-		return NULL;
-	}
-
-	fclose(file);
-	close(stat_fd);
-
-	//Ignora Kernel Threads //TODO //FIXME //HACK
-	if(flags & PF_KTHREAD){
-		free(comm);
-		return NULL;
-	}
-
-	//CALCOLO USO CPU
-	long unsigned uptime;
-	FILE* uptime_file = fopen("/proc/uptime", "r");
-	if(uptime_file){
-		ret = fscanf(uptime_file, "%lu", &uptime);
-		if(ret == EOF)
-			fprintf(stderr, "getProcessInfo: Errore Scanf Uptime");
-		else
-			cpu_usage = 100 * ((utime + stime) / ticks ) / uptime;
-		fclose(uptime_file);
-	}
-	//fine CPU
-
-	info* process_info = info_new();
-	if(!process_info){
-		free(comm);
-		return NULL;
+		return 1;
 	}
 
 	/*
@@ -132,7 +90,60 @@ info* getProcessInfo(const int dir_fd){
 	*/
 	mem = mem >> 8;
 
+	//Ignora Kernel Threads //TODO //FIXME //HACK
+	if(flags & PF_KTHREAD){
+		free(comm);
+		return 1;
+	}
+
+	//CALCOLO USO CPU
+	long unsigned uptime;
+	FILE* uptime_file = fopen("/proc/uptime", "r");
+	if(uptime_file){
+		ret = fscanf(uptime_file, "%lu", &uptime);
+		if(ret == EOF)
+			fprintf(stderr, "parseData: Errore Scanf Uptime");
+		else
+			cpu_usage = 100 * ((utime + stime) / ticks ) / uptime;
+		fclose(uptime_file);
+	}
+	//fine CPU
+
 	info_set(process_info, pid, comm, flags, (int) cpu_usage, mem); //FIXME
+	return 0;
+}
+
+info* getProcessInfo(const int dir_fd){
+	//Apertura file "stat" nella directory dir_fd (in input). dir_fd = /proc/[pid]
+	int stat_fd = openat(dir_fd, "stat", O_RDONLY);
+	if(stat_fd == -1){
+		perror("getProcessInfo: Errore openat");
+		return NULL;
+	}
+
+	FILE* stat_file = fdopen(stat_fd, "r");
+	if(!stat_file){
+		perror("getProcessInfo: Errore fdopen");
+		close(stat_fd);
+		return NULL;
+	}
+
+	info* process_info = info_new();
+	if(!process_info){
+		fclose(stat_file);
+		close(stat_fd);
+		return NULL;
+	}
+
+	int ret = parseData(stat_file, process_info);
+	if(ret){
+		fclose(stat_file);
+		close(stat_fd);
+		return NULL;
+	}
+
+	fclose(stat_file);
+	close(stat_fd);
 	return process_info;
 }
 
@@ -143,8 +154,7 @@ int filter(const struct dirent* dir){
 
 info** getProcessesList(int* len){
 	//Apertura directory /proc/
-	int proc_fd;
-	proc_fd = open("/proc/", O_RDONLY | O_DIRECTORY);
+	int proc_fd = open("/proc/", O_RDONLY | O_DIRECTORY);
 	if(proc_fd == -1){
 		perror("GetProcessesList: Errore apertura directory /proc/");
 		return NULL;
@@ -181,9 +191,8 @@ info** getProcessesList(int* len){
 		}
 		
 		//Lettura info processo
-		info* proc = getProcessInfo(pid_fd); 
-		//NOTE in caso di errore proc vale NULL
-		processes[i] = proc;
+		//NOTE in caso di errore imposta a NULL
+		processes[i] = getProcessInfo(pid_fd); 
 		
 		close(pid_fd);
 		//Dealloca singola entry
