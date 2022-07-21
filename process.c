f70d44873d5eaff305e8bc7588f238d2f3a01df1
Andrea Buonaurio <65712954+andreabuon@users.noreply.github.com>
2022-07-21 14:48:20 +0200

    Formattazione Eliminata funzione getUptime - inutile

--- process.c
+++ process.c
@@ -44,96 +44,80 @@ void info_print(const info* process_info){
 
 /*****/
 
-unsigned long getUptime(){
-	long unsigned uptime;
-	FILE* uptime_file = fopen("/proc/uptime", "r");
-	if(!uptime_file){
-		fprintf(stderr, "%s: Errore Apertura File Uptime: %s\n", __func__, strerror(errno));
-		return -1;
-	}
-	int ret = fscanf(uptime_file, "%lu", &uptime);
-	if(ret == EOF){
-		fprintf(stderr, "%s: Errore Scanf Uptime: %s\n", __func__, strerror(errno));
-		fclose(uptime_file);
-		return -1;
-	}
-	fclose(uptime_file);
-	return uptime;
-}
+long long unsigned getStatTime(){
+	/*	/proc/stat
+			  kernel/system statistics.  Varies with architecture.
+			  Common entries include:
 
-unsigned long long getStatTime(){
-	/*
-       /proc/stat
-              kernel/system statistics.  Varies with architecture.
-              Common entries include:
+			  cpu 10132153 290696 3084719 46828483 16683 0 25195 0
+			  175628 0
+			  cpu0 1393280 32966 572056 13343292 6130 0 17875 0 23933 0
+					 The amount of time, measured in units of USER_HZ
+					 (1/100ths of a second on most architectures, use
+					 sysconf(_SC_CLK_TCK) to obtain the right value),
+					 that the system ("cpu" line) or the specific CPU
+					 ("cpuN" line) spent in various states:
 
-              cpu 10132153 290696 3084719 46828483 16683 0 25195 0
-              175628 0
-              cpu0 1393280 32966 572056 13343292 6130 0 17875 0 23933 0
-                     The amount of time, measured in units of USER_HZ
-                     (1/100ths of a second on most architectures, use
-                     sysconf(_SC_CLK_TCK) to obtain the right value),
-                     that the system ("cpu" line) or the specific CPU
-                     ("cpuN" line) spent in various states:
+					 user   (1) Time spent in user mode.
 
-                     user   (1) Time spent in user mode.
+					 nice   (2) Time spent in user mode with low
+							priority (nice).
 
-                     nice   (2) Time spent in user mode with low
-                            priority (nice).
+					 system (3) Time spent in system mode.
 
-                     system (3) Time spent in system mode.
+					 idle   (4) Time spent in the idle task.  This value
+							should be USER_HZ times the second entry in
+							the /proc/uptime pseudo-file.
 
-                     idle   (4) Time spent in the idle task.  This value
-                            should be USER_HZ times the second entry in
-                            the /proc/uptime pseudo-file.
+					 iowait (since Linux 2.5.41)
+							(5) Time waiting for I/O to complete.  This
+							value is not reliable, for the following
+							reasons:
 
-                     iowait (since Linux 2.5.41)
-                            (5) Time waiting for I/O to complete.  This
-                            value is not reliable, for the following
-                            reasons:
+							1. The CPU will not wait for I/O to
+							   complete; iowait is the time that a task
+							   is waiting for I/O to complete.  When a
+							   CPU goes into idle state for outstanding
+							   task I/O, another task will be scheduled
+							   on this CPU.
 
-                            1. The CPU will not wait for I/O to
-                               complete; iowait is the time that a task
-                               is waiting for I/O to complete.  When a
-                               CPU goes into idle state for outstanding
-                               task I/O, another task will be scheduled
-                               on this CPU.
+							2. On a multi-core CPU, the task waiting for
+							   I/O to complete is not running on any
+							   CPU, so the iowait of each CPU is
+							   difficult to calculate.
 
-                            2. On a multi-core CPU, the task waiting for
-                               I/O to complete is not running on any
-                               CPU, so the iowait of each CPU is
-                               difficult to calculate.
+							3. The value in this field may decrease in
+							   certain conditions.
 
-                            3. The value in this field may decrease in
-                               certain conditions.
+					 irq (since Linux 2.6.0)
+							(6) Time servicing interrupts.
 
-                     irq (since Linux 2.6.0)
-                            (6) Time servicing interrupts.
+					 softirq (since Linux 2.6.0)
+							(7) Time servicing softirqs.
 
-                     softirq (since Linux 2.6.0)
-                            (7) Time servicing softirqs.
+					 steal (since Linux 2.6.11)
+							(8) Stolen time, which is the time spent in
+							other operating systems when running in a
+							virtualized environment
 
-                     steal (since Linux 2.6.11)
-                            (8) Stolen time, which is the time spent in
-                            other operating systems when running in a
-                            virtualized environment
+					 guest (since Linux 2.6.24)
+							(9) Time spent running a virtual CPU for
+							guest operating systems under the control of
+							the Linux kernel.
 
-                     guest (since Linux 2.6.24)
-                            (9) Time spent running a virtual CPU for
-                            guest operating systems under the control of
-                            the Linux kernel.
-
-                     guest_nice (since Linux 2.6.33)
-                            (10) Time spent running a niced guest
-                            (virtual CPU for guest operating systems
-                            under the control of the Linux kernel).
+					 guest_nice (since Linux 2.6.33)
+							(10) Time spent running a niced guest
+							(virtual CPU for guest operating systems
+							under the control of the Linux kernel).
 	*/
 	long unsigned user, nice, system, idle, iowait, irq, softirq; //steal, guest, guest_nice
+
 	FILE* stat_file = fopen("/proc/stat", "r");
 	if(!stat_file){
 		fprintf(stderr, "%s: Errore Apertura File /proc/stat: %s\n", __func__, strerror(errno));
 		return -1;
 	}
+
 	int ret = fscanf(stat_file, "%lu %lu %lu %lu %lu %lu %lu", &user, &nice, &system, &idle, &iowait, &irq, &softirq);
 	if(ret == EOF){
 		fprintf(stderr, "%s: Errore Scanf Stat: %s\n", __func__, strerror(errno));
@@ -156,8 +140,6 @@ int parseProcessData(char* path, info* process_info){
 
 	long unsigned utime1, stime1; 	//espresso in clock ticks
 	long unsigned utime2, stime2;	//espresso in clock ticks
-	long long unsigned starttime;	//espresso in secondi
-//	long ticks = sysconf(_SC_CLK_TCK);	//numero clock ticks al secondo
 	
 	/* contenuto /proc/[pid]/stat - per altre info consultare "man 5 proc"
 	(1) %d pid -> PID del processo
@@ -171,7 +153,7 @@ int parseProcessData(char* path, info* process_info){
 	(-) %* -> valori ignorati
 	*/
 	
-	/*------------------------------------FILE1--------------------------------*/
+	/*------------------------------------LETTURA1--------------------------------*/
 	//Apertura file stat
 	FILE* file1 = fopen(path, "r");
 	if(!file1){
@@ -179,8 +161,8 @@ int parseProcessData(char* path, info* process_info){
 		return -1;
 	}
 	
-	char* format_string = "%d %*[(]%m[^)]%*[)] %c %*d %*d %*d %*d %*d %u %*u %*u %*u %*u %lu %lu %*d %*d %*d %*d %*d %*d %llu %*u %ld";
-	ret = fscanf(file1, format_string, &pid, &comm, &state, &flags, &utime1, &stime1, &starttime, &rss);
+	char* format_string = "%d %*[(]%m[^)]%*[)] %c %*d %*d %*d %*d %*d %u %*u %*u %*u %*u %lu %lu %*d %*d %*d %*d %*d %*d %*u %*u %ld";
+	ret = fscanf(file1, format_string, &pid, &comm, &state, &flags, &utime1, &stime1, &rss);
 	if(ret==EOF){
 		fprintf(stderr, "%s: Errore Scanf1: %s\n", __func__, strerror(errno));
 		if(comm)
@@ -188,22 +170,20 @@ int parseProcessData(char* path, info* process_info){
 		return -1;
 	}
 	fclose(file1);
-	/*
-	long unsigned uptime1 = getUptime();
-	if(uptime1 == -1){
-		fprintf(stderr, "%s: Errore Uptime1.\n", __func__);
+	
+	long long unsigned cpu_time1 = getStatTime();
+	if(cpu_time1 == -1){
+		fprintf(stderr, "%s: Errore cpu_time1.\n", __func__);
 		return -1;
 	}
-	*/
-	long long unsigned tot_time1 = getStatTime();
-	if(tot_time1 == -1){
-		fprintf(stderr, "%s: Errore tot_time1.\n", __func__);
-		return -1;
-	}
+
 ///////
-	usleep(10000);
+
+	usleep(5000); //Aspetta un po'
+
 ///////
-	/*------------------------------------FILE2--------------------------------*/
+
+	/*------------------------------------LETTURA2--------------------------------*/
 	FILE* file2 = fopen(path, "r");
 	if(!file2){
 		fprintf(stderr, "%s: Errore apertura file /proc/%d/stat: %s\n", __func__, pid, strerror(errno));
@@ -219,30 +199,29 @@ int parseProcessData(char* path, info* process_info){
 	}
 	fclose(file2);
 
-	/*
-	unsigned long uptime2 = getUptime();
-	if(uptime2 == -1){
-		fprintf(stderr, "%s: Errore Uptime2.\n", __func__);
+	long long unsigned cpu_time2 = getStatTime();
+	if(cpu_time1 == -1){
+		fprintf(stderr, "%s: Errore cpu_time2.\n", __func__);
 		return -1;
 	}
-	*/
-	long long unsigned tot_time2 = getStatTime();
-	if(tot_time2 == -1){
-		fprintf(stderr, "%s: Errore tot_time2.\n", __func__);
-		return -1;
-	}
+
+	//--------------------------CALCOLO PERCENTUALI---------------------------// 
 
 	long long unsigned time1 = utime1 + stime1;
 	long long unsigned time2 = utime2 + stime2;
-	//long long unsigned uptime = (uptime2 - uptime1) * ticks;
-	long long unsigned interval = tot_time2 - tot_time1;
+	long long unsigned interval = cpu_time2 - cpu_time1;
 
-	printf("Processo %d - tempo usato: %llu, tempo passato: %llu .\n", pid, time2-time1, interval);
+	#ifdef DEBUG
+		printf("Processo %d - differenza: %llu, intervallo: %llu .\n", pid, time2-time1, interval);
+	#endif
 
 	float cpu_usage;
-	if(interval)
-		cpu_usage =  100 * (time2 - time1) / interval;
-	else  cpu_usage = -1.234;
+	if(interval){
+		cpu_usage =  100 * ((time2 - time1) / interval);
+	}else{
+		fprintf(stderr, "%s: Errore - intervallo = 0\n", __func__);
+		cpu_usage = -987.1234; //FIXME //PLACEHOLDER
+	}
 
 	//Conversione Memoria da Numero Pagine a MegaBytes
 	/*
